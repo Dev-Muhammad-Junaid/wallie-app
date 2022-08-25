f1ab9b11789096fea3cc27e1d330503ddcc3b9fd
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-08-26 02:41:01 +0500

    Slider Image Decoration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,23 +44,25 @@ class _HomeState extends State<Home> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+
                 CupertinoSearchTextField(
                   placeholder: "Search anything",
                   backgroundColor: Colors.white,
                   padding: EdgeInsets.all(10),
                 ),
-                SizedBox(
-                  height: 5,
-                ),
+                SizedBox(height: 25,),
+                Align(alignment:Alignment.centerLeft,child: Text("Best of the Month",textAlign: TextAlign.left,style: TextStyle(fontSize: 30,fontWeight: FontWeight.w900,color: Colors.white,),)),
+                SizedBox(height: 15,),
                 CarouselSlider(
-                  options: CarouselOptions(height: 450.0),
+                  options: CarouselOptions(height: 350.0),
                   items: imageRepository.map((i) {
                     return Builder(
                       builder: (BuildContext context) {
                         return Container(
                             width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            margin: EdgeInsets.symmetric(horizontal: 10.0),
                             decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
                                 image: DecorationImage(
                                   fit: BoxFit.cover,
                                     image: NetworkImage(imageRepository[imageRepository.indexOf(i)].url))),
@@ -69,23 +71,20 @@ class _HomeState extends State<Home> {
                     );
                   }).toList(),
                 ),
-                Container(
-                  height: 250,
-                  width: 250,
-                  child: Card(
-                    elevation: 3,
-                    child: ListTile(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      title: Text("Image"),
-                      style: ListTileStyle.drawer,
-                      leading: Image.network(
-                          'https://images.pexels.com/photos/6985001/pexels-photo-6985001.jpeg?auto=compress&cs=tinysrgb&w=800'),
-                      contentPadding: EdgeInsets.all(10),
-                      tileColor: Colors.white,
-                      selectedColor: Colors.deepOrange,
-                      subtitle: Text("Subtitle"),
-                    ),
+                SizedBox(height: 20,),
+                Card(
+                  elevation: 3,
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    title: Text("Image"),
+                    style: ListTileStyle.drawer,
+                    leading: Image.network(
+                        'https://images.pexels.com/photos/6984984/pexels-photo-6984984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                    contentPadding: EdgeInsets.all(10),
+                    tileColor: Colors.white,
+                    selectedColor: Colors.deepOrange,
+                    subtitle: Text("Subtitle"),
                   ),
                 ),
                 Card(
